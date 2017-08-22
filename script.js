aed554dae06f21f36535e02a7b02d65d9d109284
Ollie Khakwani <okhakwani@gmail.com>
2017-08-22 08:37:37 -0400

    added logic for operators, including allowing user to click one operator, change mind, click another

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(() => {
   // console.log("Hi!");
   let mainDisplayValue = "";
+  let storedOperandValue = "";
+  let storedNumberValue = "";
 
   //Handle a number button being clicked
   const handleNumberButtonClick = (e) => {
@@ -20,6 +22,26 @@ $(document).ready(() => {
     updateMainDisplay();
   }
 
+  const handleOperandButtonClick = (e) => {
+    //Get the operand clicked if there is a number entered or a stored value
+    //Or statement added to allow user to press one operand, change mind, press another
+    if(mainDisplayValue !== "" || storedNumberValue !== "") {
+      let operandValue = $(e.currentTarget).html().trim();
+      console.log("button just pressed was " + operandValue);
+
+      //Store the operand value
+      storedOperandValue = operandValue;
+
+      //And store the number value as well, but don't clear the display yet
+      //If statement added to allow user to press one operand, change mind, press another
+      if(mainDisplayValue !== "") {
+        storedNumberValue = mainDisplayValue;
+        mainDisplayValue = "";
+      }
+      console.log("storedNumberValue is " + storedNumberValue);
+    }
+  }
+
   //Update the main display value
   const updateMainDisplay = () => {
     $('.calculator__mainDisplayContent').html(mainDisplayValue);
@@ -27,6 +49,9 @@ $(document).ready(() => {
 
   //Attach number button click handler to all number buttons
   $('.calculator__button--number').on('click', handleNumberButtonClick);
+
+  //Attach operand click handler
+  $('.calculator__button--operand').on('click', handleOperandButtonClick);
 
   //Attach clear button handler
   $('.calculator__button--clear').on('click', handleClearButtonClick);
